b5fcb00f15c4e6d2e9a197fbb4061a730a657c82
Ashleigh <ashleighc207@gmail.com>
2019-06-23 11:23:31 -0400

    add README

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class Number extends React.Component {
 		return (
 			<section className="number-card">
 				<h3 className="heading-three">Your Number: {number}</h3>
-				<p className="text">{(number === 4) ? 'My favorite number!' : 'A random number, for sure. Keep trying, let\'s see if we can get your favorite nuumber!'}</p>
+				<p className="text">{(number === 4) ? 'My favorite number!' : 'A random number, for sure. Keep trying, let\'s see if we can get your favorite number!'}</p>
 				{number === 4 ? <p className="text">Not that you care.. but still</p> : <p className="text">Did you know that random number generators have been found to be not all that random after all?</p>}
 			</section>
 		)
